f4b1d59c09da38d4be31cd86c99c8744fc0768a6
hogimn <hogimn@gmail.com>
2023-06-18 12:24:04 +0900

    Fill in TODOs of assignment-wikipedia
    Wikipedia

--- wikipedia/src/main/scala/wikipedia/WikipediaData.scala
+++ wikipedia/src/main/scala/wikipedia/WikipediaData.scala
@@ -3,13 +3,27 @@ package wikipedia
 import scala.io.Source
 import scala.io.Codec
 
+/**
+ * A utility object providing functions to access and parse the Wikipedia dataset.
+ */
 object WikipediaData:
 
+  /**
+   * Retrieves the lines of the Wikipedia dataset.
+   *
+   * @return A list of strings representing the lines of the dataset.
+   */
   private[wikipedia] def lines: List[String] =
     Option(getClass.getResourceAsStream("/wikipedia/wikipedia-grading.dat")) match
       case None => sys.error("Please download the dataset as explained in the assignment instructions")
       case Some(resource) => Source.fromInputStream(resource)(Codec.UTF8).getLines().toList
 
+  /**
+   * Parses a line of the Wikipedia dataset and constructs a `WikipediaArticle` object.
+   *
+   * @param line The line to parse.
+   * @return A `WikipediaArticle` object constructed from the parsed line.
+   */
   private[wikipedia] def parse(line: String): WikipediaArticle =
     val subs = "</title><text>"
     val i = line.indexOf(subs)
